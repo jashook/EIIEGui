2762e1e7b93f2d59b478859396d5579158bcb345
Jarret Shook <shook.29@outlook.com>
2015-04-14 20:20:21 -0500

    added the process code

--- SDGUI4/SDGUI4/Page5.xaml.cs
+++ SDGUI4/SDGUI4/Page5.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Threading;
+using System.IO;
 
 namespace SDGUI4
 {
@@ -22,26 +24,67 @@ namespace SDGUI4
     public partial class Page5 : Page
     {
         private Page4 page4;
-        public String imageName;
+        public String ImageName;
+        private string OpenGLArguments;
+        private string OpenGLExeName;
+        private string EyeTrackerExeName;
+
         public Page5(String imageName, Page4 page4)
         {
             InitializeComponent();
             this.page4 = page4;
-            imageName = imageName;
+            ImageName = imageName;
+
+            string[] lines = File.ReadAllLines("install.dat");
+
+            if (lines.Count() != 4)
+            {
+               throw new Exception("Error, install.dat file has been corrupted, please reinstall");
+            }
+
+            OpenGLExeName = lines[0];
+            EyeTrackerExeName = lines[1];
+
+            StringBuilder argument_builder = new StringBuilder();
+            argument_builder.Append(ImageName);
+            argument_builder.Append(" ");
+            argument_builder.Append(lines[3]);
+            argument_builder.Append(" ");
+            argument_builder.Append(lines[4]);
+
+            OpenGLArguments = argument_builder.ToString();
         }
 
         private void Finish_Click(object sender, RoutedEventArgs e)
         {
-            Process process = new Process();
-            // Configure the process using the StartInfo properties.
-            process.StartInfo.FileName = "HelloWorld.exe"; //This is just an example file to open... could open others as well
-            process.StartInfo.Arguments = "-n";
-            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
-            process.Start();
-            process.WaitForExit();// Waits here for the process to exit.
+            new Thread(new ThreadStart( ( ) =>  
+            {
+               // Let the server (Eye Tracker) start before we open the opengl code.
+               Thread.Sleep(500);
 
-            //I think I need to use background worker to stop this application while the other runs...not entirely sure how to do this/if this is right but I found documentation on it which is promising
+               Process process = new Process();
+               // Configure the process using the StartInfo properties.
+               process.StartInfo.FileName = OpenGLExeName; //This is just an example file to open... could open others as well
+               process.StartInfo.Arguments = OpenGLArguments;
+               process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+               process.Start();
+               process.WaitForExit(); // Waits here for the process to exit.
 
+               // OpenGL Process has finished.
+            }));
+
+            new Thread(new ThreadStart(() =>
+            {
+               Process process = new Process();
+               // Configure the process using the StartInfo properties.
+               process.StartInfo.FileName = EyeTrackerExeName; //This is just an example file to open... could open others as well
+               process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+               process.Start();
+               process.WaitForExit(); // Waits here for the process to exit.
+
+               // EyeTracker has finished.
+
+            }));
 
         }
         private void Back_Click(object sender, RoutedEventArgs e)
